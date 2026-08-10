6b7f141a069dac49c06e9f70d1349138af1c29d3
SIAM <siam.khandaker@g.bracu.ac.bd>
2026-08-10 23:56:44 +0600

    restore @vite directive, re-gitignore public/build, re-add buildCommand

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-        <link rel="stylesheet" href="/css/app.css">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body style="min-height: 100vh;">
         <div style="min-height: 100vh;">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
         <title>{{ config('app.name', 'The Tenth Frame Bowling') }}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link rel="stylesheet" href="/css/app.css">
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
     </head>
     <body style="min-height: 100vh;">
 
